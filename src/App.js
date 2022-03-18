56edad2128366b204c739126233dcef3d89c9784
Stéphanie Eduarda Xavier Dias <89600726+edoardaste@users.noreply.github.com>
2022-03-18 08:33:03 -0300

    style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Tasks from "./componentes/Tasks";
 
-function App() {
+const App = () => {
+    const [tasks, setTasks] = useState([
+
+      {
+      id: '1',
+      title: 'Estudar',
+      completed: false,
+      },
+      {
+        id: '2',
+        title: 'Ler Livro',
+        completed: false,
+      },
+      {
+        id: '3',
+        title: 'Exercícios Práticos',
+        completed: false,
+      },
+
+
+
+    ]); 
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+      
+      <Tasks tasks={tasks}/>
+    
     </div>
-  );
-}
+    </>
+  ); 
+  
+};
 
 export default App;
